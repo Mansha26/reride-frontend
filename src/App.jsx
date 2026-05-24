0d25f79c9127f25ccad3c9ab8c41a83b965f7e1a
Mansha <manshaansari26@gmail.com>
2026-05-24 14:12:28 +0530

    Login page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
 import CustomRouter from './router/CustomRouter'
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
   return (
     <>
       <CustomRouter/>
+       <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+      />
     </>
   )
 }
